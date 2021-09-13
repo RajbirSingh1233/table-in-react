fa7db10cdf9ad499d3997b621688223108873a7b
Rajbir Singh <rajbirsingh1233@gmail.com>
2021-09-14 01:18:02 +0530

    first commit

---
+++ src/components/column.js
@@ -0,0 +1,29 @@
+import { ColumnFilter } from "./ColumnFilter"
+
+export const COLUMNS = [
+    {
+        Header: 'S.No.',
+        accessor: 'id',
+        Filter: ColumnFilter
+    },
+    {
+        Header: 'Name',
+        accessor: 'name',
+        Filter: ColumnFilter
+    },
+    {
+        Header: 'Class',
+        accessor: 'class',
+        Filter: ColumnFilter
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender',
+        Filter: ColumnFilter
+    },
+    {
+        Header: 'Marks of Final Year',
+        accessor: 'marks of final year',
+        Filter: ColumnFilter
+    }
+]
